e76e4722b0551c51e8ce920b1d110f22b013ecb0
davidjjeong <jaewoong.jeong@duke.edu>
2025-09-15 18:14:40 -0400

    SSR fix on hydration error + middleware for page protection

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,23 +9,25 @@ export default function Navbar() {
     ];
 
     return(
-        <nav className="relative z-50 mt-6">
-            <div className="container w-[40%] mx-auto px-6 border border-white/[.145] rounded-4xl">
-                <div className="flex items-center justify-between h-16">
-                    <Link href="/" className="flex items-center space-x-3">
-                        <span className="text-xl text-[#FFC3CC] font-bold">
-                            Guava.
-                        </span>
-                    </Link>
-                    <div className="hidden md:flex items-center space-x-8">
-                        {navLinks.map((link) => (
-                            <Link key={link.label} href={link.href} className="text-[#D2DB76] hover:text-[#FFC3CC] font-medium transition duration-300">
-                                {link.label}
-                            </Link>
-                        ))}
+        <div>
+            <nav className="relative z-50 mt-6">
+                <div className="container w-[40%] mx-auto px-6 border border-white/[.145] rounded-4xl">
+                    <div className="flex items-center justify-between h-16">
+                        <Link href="/" className="flex items-center space-x-3">
+                            <span className="text-xl text-[#FFC3CC] font-bold">
+                                Guava.
+                            </span>
+                        </Link>
+                        <div className="hidden md:flex items-center space-x-8">
+                            {navLinks.map((link) => (
+                                <Link key={link.label} href={link.href} className="text-[#D2DB76] hover:text-[#FFC3CC] font-medium transition duration-300">
+                                    {link.label}
+                                </Link>
+                            ))}
+                        </div>
                     </div>
                 </div>
-            </div>
-        </nav>
+            </nav>
+        </div>
     );
 }
